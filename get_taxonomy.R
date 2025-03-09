3b75b67a845717919afea0bed7dc5683e7f42113
Leonardo C. J. Corvalan <69471435+lcjcorvalan@users.noreply.github.com>
2025-03-09 17:02:53 -0300

    Update get_taxonomy.R
    Ajustes de comentários

--- get_taxonomy.R
+++ get_taxonomy.R
@@ -8,24 +8,27 @@ if (!requireNamespace("BiocManager", quietly = TRUE))
 BiocManager::install()
 BiocManager::valid()
 BiocManager::install(c('AnnotationDbi', 'caTools', 'edgeR', 'GenomicRanges', 'GenomicAlignments', 'gtools', 'Rsamtools', 'VennDiagram' ), ask = FALSE)
-
-#pacote
 install.packages("myTAI")
+
+#packages
 require(myTAI)
 
-# If you hava a lot specie, use the API key from ncbi
-#se API key, só use se for rodar para muitas espécies mais de 5k
+
+# If you have many species, I recommend using the ncbi API key
+# to generate your API kry acess: https://account.ncbi.nlm.nih.gov/settings/
+
+# TO use the api key
 taxize::use_entrez()
 ENTREZ_KEY='552717a8bf15a10e0810fe042905dd5f3808'
+
 #copy and paste the key on the .Renvirion
 usethis::edit_r_environ()
-.rs.restartR()
+.rs.restartR() # restart R
 
 # read the datas
-
 library(readxl)
 
-ssp <- read_xlsx('Ncbi_mito_list.xlsx')
+ssp <- read_xlsx('Ncbi_mito_list_mini.xlsx')
 ssp
 
 # create a function for get the family  information for each specie. -> cat faily
@@ -34,6 +37,7 @@ catfamily <- function(x){
   tax <- taxonomy(x, db = "ncbi", output = "classification")
   family <- tax[tax$rank=="family",]
 }
+
 ## apply the catfamily function in loop for all listed species 
 
 data <- lapply(ssp$ssp, catfamily)
@@ -47,22 +51,25 @@ for (i in 1:length(data)) {
     }
 }
 
+# marge datas
 data_total <- do.call(rbind, data)
 family_data <- (data_total$name)
 family_data
 
+#  add family information to the DF
 ssp$family<- family_data
 
-#procurar ordens 
+# Get order informations
+
 catorder <- function(x){
   tax <- taxonomy(x, db = "ncbi", output = "classification")
   order <- tax[tax$rank=="order",]
 }
 
-
-## Aplicando a função catorder e obtendo os nomes das ordem a que percentecem cada familia em estudo:
+#Applying the catorder function and obtaining the names of the orders in which each family under study belongs:
 
 data_order <- lapply(ssp$ssp, catorder)
+
 # transform the species with out data in NA
 
 for (i in 1:length(data_order)) {
@@ -78,4 +85,5 @@ order_data
 
 ssp$order<- order_data
 
+# save df in csv file
 write.csv(ssp, file = "spcies_list_taxonomy_information.csv")
